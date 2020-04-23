f0a3dd60dc816cbd1a2d70922f9871c2690e0463
Maxime Deboffle <maxime.deboffle@hotmail.com>
2020-04-23 20:08:08 +0200

    Connection Button Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './layouts/root/app.component';
-//import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule } from '@angular/material/menu'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MDBBootstrapModule, WavesModule  } from 'angular-bootstrap-md'
 //import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 //import { MatRadioModule } from '@angular/material/radio'
 //import { MatButtonModule } from '@angular/material/button'
@@ -13,11 +15,10 @@ import { AppComponent } from './layouts/root/app.component';
 //import { MatNativeDateModule } from '@angular/material/core'
 //import { MatCardModule } from '@angular/material/card'
 //import { MatToolbarModule } from '@angular/material/toolbar'
-//import { MatMenuModule } from '@angular/material/menu'
 //import { MatFormFieldModule } from '@angular/material/form-field'
 //import { MatSnackBarModule } from '@angular/material/snack-bar'
 //import { MatDialogModule } from '@angular/material/dialog'
-//import { MatIconModule } from '@angular/material/icon'
+import { MatIconModule } from '@angular/material/icon'
 
 import { SocialLoginModule, AuthServiceConfig, LoginOpt } from "angularx-social-login";
 import { GoogleLoginProvider } from "angularx-social-login";
@@ -25,6 +26,7 @@ import { HeaderComponent } from './layouts/header/header.component';
 import { BodyComponent } from './layouts/body/body.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
+import { ConnectionBtnComponent } from './components/connection-button/connection-button.component';
 
 import { AppRoutingModule } from './router/app-routing.module';
 import { environment } from 'src/environments/environment';
@@ -68,7 +70,8 @@ export function provideConfig() {
     BeersListComponent,
     BeersCardComponent,
     ShopsListComponent,
-    ShopsCardComponent
+    ShopsCardComponent,
+    ConnectionBtnComponent
   ],
   imports: [
     SocialLoginModule,
@@ -82,6 +85,11 @@ export function provideConfig() {
     MatCardModule,
     MatButtonModule,
     MatToolbarModule,
+    MatMenuModule,
+    BrowserAnimationsModule,
+    MDBBootstrapModule.forRoot(),
+    WavesModule,
+    MatIconModule
   ],
   providers: [
     {
---
+++ src/app/components/connection-button/connection-button.component.ts
@@ -0,0 +1,51 @@
+import { Component } from "@angular/core";
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { AppState } from 'src/app/store/app.state';
+import { Store } from '@ngrx/store';
+import { User } from 'src/app/models/User';
+
+@Component({
+    selector: 'connection-button',
+    templateUrl: './connection-button.component.html',
+    styleUrls: ['./connection-button.component.css']
+})
+export class ConnectionBtnComponent{
+    user: User = null;
+    token: string = null;
+
+    constructor(
+        private authService: AuthenticationService,
+        private store: Store<AppState>,
+    ){
+        this.store.select('user').subscribe(user => this.user = user);
+        this.store.select('token').subscribe(token => this.token = token);
+    }
+
+    get connectBtnText() : String {
+        return this.loggedIn ? this.user.name : "";
+    }
+
+    get userName(): string {
+        return this.loggedIn ? this.user.name : "";
+    }
+
+    get loggedIn(): boolean {
+        return this.user != null;
+    }
+
+    get avatarUrl(): string {
+        return this.loggedIn ? this.user.avatarUrl : "https://static.thenounproject.com/png/363639-200.png";
+    }
+
+    async connectionMethod(): Promise<void> {
+        this.loggedIn ? this.signOut() : this.signInWithGoogle();
+    }
+  
+    async signInWithGoogle(): Promise<void> {
+        await this.authService.signIn();
+    }
+
+    async signOut(): Promise<void> {
+        await this.authService.signOut();
+    }
+}
