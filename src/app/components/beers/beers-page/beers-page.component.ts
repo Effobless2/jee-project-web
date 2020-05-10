e08516c20487d5ff7124e7e95755940881d090ec
Maxime Deboffle <maxime.deboffle@hotmail.com>
2020-05-10 17:38:56 +0200

    Setup beers' filter formular

--- src/app/components/beers/beers-page/beers-page.component.ts
+++ src/app/components/beers/beers-page/beers-page.component.ts
@@ -2,52 +2,94 @@ import { Component } from "@angular/core";
 import { Beer } from 'src/app/models/Beer';
 import { BeerService } from 'src/app/services/api/beer.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { FormGroup, FormBuilder } from '@angular/forms';
+
+interface SearchBeerFormularFields {
+  name?: string;
+}
 
 @Component({
-    selector: 'beers-page',
-    templateUrl: 'beers-page.component.html',
-    styleUrls: ['beers-page.component.css']
+  selector: 'beers-page',
+  templateUrl: 'beers-page.component.html',
+  styleUrls: ['beers-page.component.css']
 })
 export class BeersPageComponent {
-    beers: Beer[] = [
-        {name: "Leffe Blonde",
-          profilePict: "../../assets/leffe_blonde.jpg",
-          type: "Bière Blonde",
-          alcoholLevel: 5,
-          description: "Bonne binouze quand elle est fraiche"},
-        {name: "Leffe Ruby",
-          profilePict: "../../assets/image_218.jpg",
-          type: "Bière Rouge",
-          alcoholLevel: 5,
-          description: "Meilleure binouze <3"},
-        {name: "Kro",
-          profilePict: "../../assets/leffe_blonde.jpg",
-          type: "Bière Nulle",
-          alcoholLevel: 4,
-          description: "..."},
-        {name: "Delirium",
-          profilePict: "../../assets/leffe_blonde.jpg",
-          type: "Bière Blonde",
-          alcoholLevel: 7,
-          description: "Tres tres bon"},
-        {name: "Chouffe",
-          profilePict: "../../assets/image_218.jpg",
-          type: "Bière",
-          alcoholLevel: 8,
-          description: "Good"},
-        {name: "Grim Red",
-          profilePict: "../../assets/image_218.jpg",
-          type: "Bière Rouge",
-          alcoholLevel: 5,
-          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sagittis tellus ex, sed tincidunt diam pellentesque nec. Pellentesque ac mi tortor. Aenean magna ipsum, interdum eu lorem in, mattis interdum sapien. Sed elit orci, varius nec varius vitae, auctor a dui. Etiam quis dui odio. Pellentesque lobortis dolor non tristique facilisis. Integer porttitor nulla a dolor tempus, eget venenatis dui blandit. Curabitur eu facilisis massa. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut dapibus ultricies mi sit amet aliquet. Vestibulum vitae turpis eu nisl tempus scelerisque. Maecenas ut ligula ex."}
-      ];
+  formGroup: FormGroup;
+  beers: Beer[] = [
+    {
+      name: "Leffe Blonde",
+      profilePict: "../../assets/leffe_blonde.jpg",
+      type: "Bière Blonde",
+      alcoholLevel: 5,
+      description: "Bonne binouze quand elle est fraiche"
+    },
+    {
+      name: "Leffe Ruby",
+      profilePict: "../../assets/image_218.jpg",
+      type: "Bière Rouge",
+      alcoholLevel: 5,
+      description: "Meilleure binouze <3"
+    },
+    {
+      name: "Kro",
+      profilePict: "../../assets/leffe_blonde.jpg",
+      type: "Bière Nulle",
+      alcoholLevel: 4,
+      description: "..."
+    },
+    {
+      name: "Delirium",
+      profilePict: "../../assets/leffe_blonde.jpg",
+      type: "Bière Blonde",
+      alcoholLevel: 7,
+      description: "Tres tres bon"
+    },
+    {
+      name: "Chouffe",
+      profilePict: "../../assets/image_218.jpg",
+      type: "Bière",
+      alcoholLevel: 8,
+      description: "Good"
+    },
+    {
+      name: "Grim Red",
+      profilePict: "../../assets/image_218.jpg",
+      type: "Bière Rouge",
+      alcoholLevel: 5,
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sagittis tellus ex, sed tincidunt diam pellentesque nec. Pellentesque ac mi tortor. Aenean magna ipsum, interdum eu lorem in, mattis interdum sapien. Sed elit orci, varius nec varius vitae, auctor a dui. Etiam quis dui odio. Pellentesque lobortis dolor non tristique facilisis. Integer porttitor nulla a dolor tempus, eget venenatis dui blandit. Curabitur eu facilisis massa. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut dapibus ultricies mi sit amet aliquet. Vestibulum vitae turpis eu nisl tempus scelerisque. Maecenas ut ligula ex."
+    }
+  ];
 
-      
-  constructor(private beerService: BeerService) {
+
+  constructor(
+    private beerService: BeerService,
+    
+    private formBuilder: FormBuilder) {
+    this.formGroup = this.formBuilder.group({
+      name: null
+    } as SearchBeerFormularFields);
+
     this.beerService.getAll(
-      (beers: Beer[]) => {if(beers != []) this.beers = beers;},
+      (beers: Beer[]) => {
+        if (beers && beers.length !== 0) {
+
+          console.log(beers);
+          this.beers = beers;
+        }
+      },
       (error: HttpErrorResponse) => console.error(error)
     );
-   }
+  }
+
+  onSearch() {
+    let searchFilters: SearchBeerFormularFields = this.formGroup.value as SearchBeerFormularFields;
+    console.log(searchFilters); 
+  }
+
+  get disabled(): boolean {
+    return Object.values(this.formGroup.value as SearchBeerFormularFields).every((value: any) => {
+      return !value;
+    });
+  }
 
 }
