42b34a217c2d25bcb6463f92af2b90ef7b0fb151
Maxime Deboffle <maxime.deboffle@hotmail.com>
2020-05-10 19:14:15 +0200

    User's datas stored in localStorage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,7 +33,8 @@ import { ConnectionBtnComponent } from './components/connection-button/connectio
 import { AppRoutingModule } from './router/app-routing.module';
 import { environment } from 'src/environments/environment';
 
-import { StoreModule } from '@ngrx/store'
+import { StoreModule, ActionReducerMap, ActionReducer, MetaReducer } from '@ngrx/store'
+import { localStorageSync } from 'ngrx-store-localstorage';
 import { userReducer } from './store/reducers/user.reducer';
 import { tokenReducer } from './store/reducers/token.reducer';
 import { AuthenticationService } from './services/authentication.service';
@@ -60,6 +61,7 @@ import { GeolocationService } from "./services/geolocation.service";
 import { MatDialogModule } from '@angular/material/dialog';
 import { AddressListModalComponent } from './components/forms/shop-formular/modal/address-list-modal.component';
 import { BeersPageComponent } from './components/beers/beers-page/beers-page.component';
+import { AppState } from './store/app.state';
 
 const googleLoginOptions: LoginOpt = {
   scope: 'profile email'
@@ -75,6 +77,15 @@ let config = new AuthServiceConfig([
 export function provideConfig() {
   return config;
 }
+
+const reducers: ActionReducerMap<AppState> = {
+  user: userReducer,
+  token: tokenReducer
+};
+export function localStorageSyncReducer(reducer: ActionReducer<any>): ActionReducer<any> {
+  return localStorageSync({keys: ['user', 'token'], rehydrate: true})(reducer);
+}
+const metaReducers: Array<MetaReducer<any, any>> = [localStorageSyncReducer];
 
 
 @NgModule({
@@ -100,10 +111,10 @@ export function provideConfig() {
     SocialLoginModule,
     BrowserModule,
     HttpClientModule,
-    StoreModule.forRoot({
-      user: userReducer,
-      token: tokenReducer
-    }),
+    StoreModule.forRoot(
+      reducers,
+      {metaReducers}
+    ),
     AppRoutingModule,
     MatProgressBarModule,
     MatCardModule,
