2174eeb410c499f3180d5d1293c2a402b93d41cf
Kaalawolf <dabinmegane@gmail.com>
2020-06-03 19:14:21 +0200

    shop filter front ok

--- src/app/components/forms/shop-search-bar/shop-search-bar.component.ts
+++ src/app/components/forms/shop-search-bar/shop-search-bar.component.ts
@@ -1,13 +1,19 @@
 import { Component, Input } from "@angular/core";
-import { FormArray, FormGroup, FormBuilder } from '@angular/forms';
+import { FormArray, FormGroup, FormBuilder, FormControl } from '@angular/forms';
 import { tradeTypes } from 'src/app/services/tools/trade.types';
-import { TradeSearchService, TradeSearchDatas } from 'src/app/services/api/tradeSearch.service';
+import { TradeSearchService, TradeSearchData } from 'src/app/services/api/tradeSearch.service';
 import { Trade } from 'src/app/models/Trade';
+import { GeolocationService } from "../../../services/geolocation.service";
+import { GeocodingService } from "../../../services/geocoding.service";
+import { GeocoderResult } from "@agm/core";
 
 interface SearchShopFormularFields {
   name: string;
-  types: FormArray | boolean[]; //for getting datas, it seems to be transformed as a boolean array
+  types: FormArray | boolean[]; //for getting data, it seems to be transformed as a boolean array
   address: string;
+  nearMe: FormControl;
+  lng: number;
+  lat: number;
 }
 
 function multipleCheckboxRequireOne(fa: FormArray) {
@@ -37,7 +43,13 @@ export class ShopSearchBarComponent {
   tradeTypes: string[] = tradeTypes;
 
   constructor(private tradeSearchService: TradeSearchService,
-              private formBuilder: FormBuilder) {
+              private formBuilder: FormBuilder,
+              private geolocationService: GeolocationService,
+              private geocodingService: GeocodingService) {
+    this.initFormGroups();
+  }
+
+  initFormGroups() {
     this.shopTypesForm = this.formBuilder.array(
       this.tradeTypes.map((value: string) => this.formBuilder.control(false)),
       [multipleCheckboxRequireOne]
@@ -45,24 +57,82 @@ export class ShopSearchBarComponent {
     this.formGroup = this.formBuilder.group({
       name: null,
       types: this.shopTypesForm,
-      address: null
+      address: null,
+      nearMe: new FormControl(true)
     } as SearchShopFormularFields);
   }
 
+  _rollback() {
+    this.initFormGroups();
+    this.rollback();
+  }
+
   onSearch() {
-    let searchFilters: SearchShopFormularFields = this.formGroup.value as SearchShopFormularFields;
-    let searchDatas: TradeSearchDatas = {
-      name: searchFilters.name,
-      types: tradeTypes.filter((value: string, index: number) => searchFilters.types[index]),
-      address: searchFilters.address
+    if ((this.formGroup.value as SearchShopFormularFields).nearMe) {
+      this.geolocationService.getCurrentPosition(this.geoPositionCallback.bind(this), this.geolocationError);
+    } else if ((this.formGroup.value as SearchShopFormularFields).address) {
+      this.geocodingService.findLocation(
+        (this.formGroup.value as SearchShopFormularFields).address,
+        this.geoLocationCallback.bind(this),
+        this.geolocationError
+      );
+    } else {
+      this.sendRequest();
     }
-    this.tradeSearchService.get(searchDatas, this.callback);
   }
 
   get disabled(): boolean {
+    if ((this.formGroup.value as SearchShopFormularFields).nearMe) {
+        return false;
+    }
     return Object.values(this.formGroup.value as SearchShopFormularFields).every((value: string | FormArray) => {
       return typeof (value) === "string" && !value ||
         typeof (value) !== "string" && !this.shopTypesForm.valid;
     });
+  }
+
+  positionWrapper(p: Position): {lng: number, lat: number} {
+    return {lng: p.coords.longitude, lat: p.coords.latitude};
+  }
+
+  geocoderResultWrapper(res: GeocoderResult): {lng: number, lat: number} {
+    return {lng: res.geometry.location.lng(), lat: res.geometry.location.lat()};
+  }
+
+  geoPositionCallback(p: Position) {
+    let pos = this.positionWrapper(p);
+    this.sendRequest(pos);
+  }
+
+  geoLocationCallback(res: GeocoderResult[]) {
+    if (res.length == 0) {
+      this.geolocationError();
+    } else {
+      let pos = this.geocoderResultWrapper(res[0]);
+      this.sendRequest(pos);
+    }
+  }
+
+  sendRequest(p?: {lng: number, lat: number}) {
+    let searchFilters: SearchShopFormularFields = this.formGroup.value as SearchShopFormularFields;
+    let searchData: TradeSearchData;
+    if (p) {
+      searchData = {
+        name: searchFilters.name,
+        types: tradeTypes.filter((value: string, index: number) => searchFilters.types[index]),
+        lng: p.lng,
+        lat: p.lat
+      };
+    } else {
+      searchData = {
+        name: searchFilters.name,
+        types: tradeTypes.filter((value: string, index: number) => searchFilters.types[index])
+      };
+    }
+    this.tradeSearchService.get(searchData, this.callback);
+  }
+
+  geolocationError() {
+    window.alert("Activer la géolocalisation ou entrer une adresse.");
   }
 }
--- src/app/services/api/tradeSearch.service.ts
+++ src/app/services/api/tradeSearch.service.ts
@@ -4,20 +4,21 @@ import { HttpService } from '../tools/http.service';
 import { Trade } from 'src/app/models/Trade';
 import { HttpErrorResponse } from '@angular/common/http';
 
-export interface TradeSearchDatas{
-  name: string;
+export interface TradeSearchData{
+  name?: string;
   types?: string[];
-  address?: string;
+  lng?: number;
+  lat?: number;
 }
 
 @Injectable()
 export class TradeSearchService {
-  private controllerUrl: string = `${environment.beererApiUrl}trade/search`;
+  private controllerUrl: string = `${environment.beererApiUrl}trades/search`;
 
   constructor(private httpService: HttpService){}
 
-  get(datas: TradeSearchDatas, callback: (result: Trade[]) => any, error?: (err: HttpErrorResponse) => any){
-    return this.httpService.get(`${this.controllerUrl}?name=${datas.name ?? "" }&types=${datas.types ?? "" }&address=${datas.address ?? ""}`)
+  get(data: TradeSearchData, callback: (result: Trade[]) => any, error?: (err: HttpErrorResponse) => any){
+    return this.httpService.get(`${this.controllerUrl}?name=${data.name ?? "" }&types=${data.types ?? "" }&lng=${data.lng ?? ""}&lat=${data.lat ?? ""}`)
       .subscribe({
         next: callback,
         error: error
